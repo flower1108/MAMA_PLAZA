84a06d5b72930e3a5639ea231b55a9e225d6286e
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-06-07 04:52:15 +0000

    AddController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'followers/create'
+  get 'followers/destroy'
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
+  get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
